8ebab38d5dedd107466ec5d499c9bc643620b7f5
Camilla Landin <landin.camilla8@gmail.com>
2024-02-29 14:35:23 +0100

    Adding userJoinReq to SocketTypes

--- shared/types/SocketTypes.ts
+++ shared/types/SocketTypes.ts
@@ -2,8 +2,10 @@ export {}
 
 // Events emitted by the server to the client
 export interface ServerToClientEvents {
+
 }
 
 // Events emitted by the client to the server
 export interface ClientToServerEvents {
+    userJoinReq: (username: string, callback: (success: boolean) => void) => void;
 }
