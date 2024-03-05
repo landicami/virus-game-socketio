b33931aef75ac6538e2487f83feddd9ff299ece9
Camilla Landin <landin.camilla8@gmail.com>
2024-03-05 09:11:46 +0100

    Made heroku-changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,27 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
-import routes from "./routes";
+import path from "path";
+
+// Detect environment
+const NODE_ENV = process.env.NODE_ENV || "development";
+
+// Path to frontend build
+const frontendDistPath = path.join(__dirname, NODE_ENV === "production"
+	? "../../../../frontend/dist"
+	: "../../frontend/dist"
+);
+
+console.log("🌱 Environment:", NODE_ENV);
+console.log("🚚 Path to frontend build:", frontendDistPath);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// Use dem routes
-app.use(routes);
+// Serve frontend
+app.use(express.static(frontendDistPath));
+
 
 export default app;
